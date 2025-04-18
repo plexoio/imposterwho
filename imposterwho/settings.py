172af054845f18add5e48773f92a1489e8eb4b0a
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2025-04-18 11:17:47 +0000

    fix: Correct Secret Key

--- imposterwho/settings.py
+++ imposterwho/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-faq=pi3cxzjcpulu!b8r&7cnc386l)&=f@m^3g_g6*emd0(qt$"
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 IN_DEVELOPMENT = os.environ.get("IN_DEVELOPMENT", "False").strip().lower() == "true"
