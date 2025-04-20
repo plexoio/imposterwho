edbd337fa1615e3870abc61bc936bd374b4d4bf5
mogr20 <markjordanrandall@gmail.com>
2025-04-20 15:06:48 +0100

    ADD: basic draggable element tutorial using interactjs, next dragzone

--- imposterwho/settings.py
+++ imposterwho/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     # apps
     "admin_dashboard",
     "homepage",
+    "game",
 ]
 
 
--- imposterwho/urls.py
+++ imposterwho/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('manager/', include('admin_dashboard.urls')),
     path("accounts/", include("allauth.urls")),
+    path('game/', include('game.urls')),
     path('', include('homepage.urls'), name='home'), 
 ]
