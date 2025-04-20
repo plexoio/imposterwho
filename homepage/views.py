b571c6eb6e20aa2c2f3c6950b8eab0694c8a0ba0
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2025-04-20 17:58:08 +0000

    fix: Empty .log

--- homepage/views.py
+++ homepage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 class HomePage(TemplateView):
-    template_name = 'homepage/index.html'
+    template_name = "homepage/index.html"
