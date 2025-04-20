b6f94e8b5e045912d9708b33a6311bcf99182e07
RichardM <114283158+richard9106@users.noreply.github.com>
2025-04-20 13:26:45 +0200

    feat: Advance in admin & user dashboard as well as the homepage
    * initial commit richard branch
    
    * basic structure admin dashboard
    
    * logs
    
    * admin dashboard
    
    * Fix adding categories + edit content to english
    
    * Home page edit
    
    ---------
    
    Co-authored-by: Richard Marles <richard.foticos@gmail.com>

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
 
 
 class QuoteType(models.Model):
     """Quote types"""
     name = models.CharField(max_length=100, unique=True)  # Ej: "Genérico"
     slug = models.SlugField(unique=True)  # Ej: "generic", "programming"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
